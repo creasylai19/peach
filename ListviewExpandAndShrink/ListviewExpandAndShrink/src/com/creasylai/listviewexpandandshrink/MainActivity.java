42f94c7f3c1c6f510856b8647d568e4c11d74971
creasylai19 <creasylai19@126.com>
2015-10-29 00:43:03 +0800

    seperate lib and demo
    use a footadapter implement listview expand and shrink function

---
+++ ListviewExpandAndShrink/ListviewExpandAndShrink/src/com/creasylai/listviewexpandandshrink/MainActivity.java
@@ -0,0 +1,30 @@
+package com.creasylai.listviewexpandandshrink;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ArrayList<StationImage> mStationImages = new ArrayList<StationImage>();
+		ListView listview = null;
+		for (int i = 0; i < 5; i++) {
+			StationImage mStationImage = new StationImage();
+			mStationImage.setImg_name("第" + i + "个项目的名字");
+			mStationImage.setImg_url_l("第" + i + "个项目的长URL");
+			mStationImage.setImg_url_s("第" + i + "个项目的短URL");
+			mStationImages.add(mStationImage);
+		}
+		listview = (ListView) findViewById(R.id.listview);
+		FootviewAdapter mFootviewAdapter = new FootviewAdapter(this, listview);
+		listview.setAdapter(mFootviewAdapter);
+		mFootviewAdapter.setAdapterData(mStationImages);
+	}
+
+}
